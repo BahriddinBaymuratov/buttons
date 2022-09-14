a45e7f783fc6f96008f3346a5fd80a2f195fc361
BahriddinBaymuratov <boymurodov.070.bahriddin@gmail.com>
2022-09-14 11:26:16 +0300

    Buttons

---
+++ app/src/main/java/com/example/buttons/activity_tasbeh.kt
@@ -0,0 +1,40 @@
+package com.example.buttons
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//import android.widget.Button
+//import android.widget.ImageButton
+//import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+//    private var count = 0
+//
+//    private lateinit var textNumber: TextView
+//    private lateinit var btnPlus: Button
+//    private lateinit var btnRefresh: ImageButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+//        initViews()
+        }
+
+//    private fun initViews(){
+//
+//        textNumber = findViewById(R.id.textNumber)
+//        btnPlus = findViewById(R.id.btnPlus)
+//        btnRefresh = findViewById(R.id.btnRefresh)
+//
+//        btnPlus.setOnClickListener(){
+//            count++
+//            textNumber.text = count.toString()
+//        }
+//        btnRefresh.setOnClickListener(){
+//            textNumber.text = "0"
+//            count = 0
+//        }
+//    }
+}
